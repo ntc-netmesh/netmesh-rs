7ca687bdd43de01e93cd9a7de6c042cc0f72d8ad
Claire Barela <claire.barela@gmail.com>
2019-07-14 20:33:45 +0800

    templates and views to list test agents

--- netmesh_web/urls.py
+++ netmesh_web/urls.py
@@ -1,19 +1,16 @@
 from django.urls import path
 from netmesh_web.views import index
-from netmesh_web.views import dashboard
-import django
+from netmesh_web.views import users
 
 
 urlpatterns = []
-#
-# """ Auth """
-# urlpatterns += [
-#     path('login/', django.contrib.auth.views.login, {'template_name': 'accounts/login.html'}, name="login"),
-#     path('logout/', django.contrib.auth.views.logout, {'template_name': 'accounts/logout.html'}, name="logout"),
-# ]
 
-
+""" Main results page """
 urlpatterns += [
     path('', index.home, name='index'),
-    path('ne/', dashboard.home, name='home')
+]
+
+""" Agents """
+urlpatterns += [
+    path('agents/', users.agent_list, name='agents')
 ]

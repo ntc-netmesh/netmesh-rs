10256ae5b492859b028e762bda31173763ee5854
Claire Barela <claire.barela@gmail.com>
2019-07-14 16:56:49 +0800

    api improvements - ensure auth classes are present in SUBMIT api - clean REGISTER api

--- netmesh_api/views/api1.py
+++ netmesh_api/views/api1.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.authentication import (TokenAuthentication,
-                                           SessionAuthentication)
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import JSONRenderer
 from rest_framework.authtoken.models import Token
@@ -24,6 +24,8 @@ class SubmitData(APIView):
         <base_url>/api/submit/?
     """
     renderer_classes = (JSONRenderer,)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     @staticmethod
     def get_client_ip(request):
@@ -72,26 +74,31 @@ class SubmitData(APIView):
 
 
 class Register(APIView):
+    """ API endpoint to get Agent token
+        <base_url>/api/register/?
+    """
+    renderer_classes = (JSONRenderer,)
 
     def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
-        uuid = request.data.get("uuid")
+        username = request.data["username"]
+        password = request.data["password"]
+        uuid = request.data["uuid"]
 
         try:
             AgentProfile.objects.get(uuid=uuid)
         except ObjectDoesNotExist:
-            return Response("ERROR: Invalid Agent ID",
-                                status=status.HTTP_400_BAD_REQUEST)
-
+            return Response("ERROR: Invalid Agent ID", status=status.HTTP_400_BAD_REQUEST)
 
         if username is None or password is None:
-            return Response({'error': 'Please provide both username and password'},
+            return Response({'ERROR': 'Please provide both username and password'},
                             status=status.HTTP_400_BAD_REQUEST)
+
         user = authenticate(username=username, password=password)
+
         if not user:
-            return Response({'error': 'Invalid Credentials'},
-                            status=status.HTTP_404_NOT_FOUND)
+            return Response({'ERROR': 'Invalid Credentials'}, status=status.HTTP_404_NOT_FOUND)
+
         token, _ = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key},
+
+        return Response({'Token': token.key},
                         status=status.HTTP_200_OK)
